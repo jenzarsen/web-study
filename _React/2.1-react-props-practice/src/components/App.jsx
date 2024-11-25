88baaaca391098bf98afe8ae29bc3cda331fecb9
jenzarsen <jenzarsen.alabado@gmail.com>
2024-11-25 11:17:54 +0800

    Finish 2.1 React Props Practice

--- _React/2.1-react-props-practice/src/components/App.jsx
+++ _React/2.1-react-props-practice/src/components/App.jsx
@@ -1,22 +1,35 @@
 import React from "react";
+import "../styles.css";
+import contacts from "../contacts.js";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.contact.name}</h2>
+        <img
+        className ="circle-img"
+          src={props.contact.imgURL}
+          alt="avatar_img"
+        />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.contact.phone}</p>
+        <p className="info">{props.contact.email}</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  var contactList = contacts;
+
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card contact={contactList[0]} />
+       <Card contact={contactList[1]} />
+        <Card contact={contactList[2]} />
     </div>
   );
 }
