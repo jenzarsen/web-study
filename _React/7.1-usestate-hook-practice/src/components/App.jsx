0e3d2034d8e078521642b6435dbc197052e510d7
jenzarsen <jenzarsen.alabado@gmail.com>
2024-11-27 14:42:14 +0800

    7.1 Final React useState Practice

--- _React/7.1-usestate-hook-practice/src/components/App.jsx
+++ _React/7.1-usestate-hook-practice/src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString());
+  }
+
+   setInterval(getTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
