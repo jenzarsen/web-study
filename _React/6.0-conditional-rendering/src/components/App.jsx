8931b70befb4e869986892eb3bf1da58abe34e4a
jenzarsen <jenzarsen.alabado@gmail.com>
2024-11-27 10:01:41 +0800

    Finalize 6.0 Conditional Rendering

---
+++ _React/6.0-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./Login.jsx";
+
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1> Hello </h1>;
+  } else {
+    return <Login />;
+  }
+}
+
+function App() {
+  return <div className="container">{isLoggedIn? <h1> Hello </h1> : <Login /> }</div>;
+}
+
+export default App;
