eae5a9bf85cefce0976b62eb900cc2249c1425ab
jenzarsen <jenzarsen.alabado@gmail.com>
2024-11-29 11:31:53 +0800

    Finalize 10.0 React Forms

--- _React/10.0-react-forms/src/components/App.jsx
+++ _React/10.0-react-forms/src/components/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [isClicked, setClicked] = useState(false);
+  function handleChange(event) {
+    setName(event.target.value);
+    setClicked(false);
+  }
+
+  function handleClick(event) {
+    setClicked(true);
+    event.preventDefault();
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {isClicked && name}</h1>
+      <form>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit" onClick={handleClick}>Submit</button>
+      </form>
     </div>
   );
 }
